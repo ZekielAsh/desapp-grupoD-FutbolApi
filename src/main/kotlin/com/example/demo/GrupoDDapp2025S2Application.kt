9ed0284e83ca200f516fbc2906351d2a65d9ae93
Julian Bensal <julianbensal@gmail.com>
2025-09-10 19:16:26 -0300

    init

---
+++ src/main/kotlin/com/example/demo/GrupoDDapp2025S2Application.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrupoDDapp2025S2Application
+
+fun main(args: Array<String>) {
+	runApplication<GrupoDDapp2025S2Application>(*args)
+}
